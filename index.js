b19dccb9abe24e437e22282d6149aaee291f3720
vipul-khedekar <3kvipul@gmail.com>
2022-06-02 20:06:13 +0530

    created method for updating weather image as change in climate, minor grammatical and typos correction in weather info

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ class Weather {
                     const climate = response.weather[0].main;
 
                     this.updateWeatherInfo(cityName, temperature, feelsLike, clouds, wind, climate);                    
+                    this.updateClimateImg(climate);
                 });
         }
         catch (err) {
@@ -41,7 +42,55 @@ class Weather {
 
     updateWeatherInfo(cityName, temperature, feelsLike, clouds, wind, climate) {
         weatherInfo.innerText = `In ${cityName}, temperature is ${temperature}°C but feels like ${feelsLike}°C.
-                                It's ${clouds}% cloudy, with wind speed of ${wind} and the climate is ${climate}.`;
+                                It's ${clouds}% cloudy, with wind speed of ${wind}m/s and the climate state is ${climate} like.`;
+    }
+
+    updateClimateImg(climate) {
+        if(climate === `Thunderstorm`) {
+            climateImg.src = `https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/undefined/external-thunderstorm-weather-vitaliy-gorbachev-lineal-color-vitaly-gorbachev-3.png`;
+        }
+        if(climate === `Drizzle`) {
+            climateImg.src = `https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/undefined/external-drizzle-weather-flaticons-lineal-color-flat-icons.png`;
+        }
+        if(climate === `Rain`) {
+            climateImg.src = `https://img.icons8.com/dusk/64/undefined/rain--v1.png`;
+        }
+        if(climate === `Snow`) {
+            climateImg.src = `https://img.icons8.com/dusk/64/undefined/snow.png`;
+        }
+        if(climate === `Clear`) {
+            climateImg.src = `https://img.icons8.com/external-kosonicon-lineal-color-kosonicon/64/undefined/external-clear-sky-weather-kosonicon-lineal-color-kosonicon.png`;
+        }
+        if(climate === `Clouds`) {
+            climateImg.src = `https://img.icons8.com/external-justicon-lineal-color-justicon/64/undefined/external-cloudy-weather-justicon-lineal-color-justicon-1.png`;
+        }
+        if(climate === `Mist`) {
+            climateImg.src = `https://img.icons8.com/dusk/64/undefined/foggy-night-1.png`;
+        }
+        if(climate === `Smoke`) {
+            climateImg.src = `https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/undefined/external-smoke-factory-flaticons-lineal-color-flat-icons-2.png`;
+        }
+        if(climate === `Haze`) {
+            climateImg.src = `https://img.icons8.com/plasticine/100/undefined/foggy-night-1.png`;
+        }
+        if(climate === `Dust`) {
+            climateImg.src = `https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/undefined/external-dust-weather-flaticons-lineal-color-flat-icons.png`;
+        }
+        if(climate === `Fog`) {
+            climateImg.src = `https://img.icons8.com/external-fauzidea-flat-fauzidea/64/undefined/external-fog-weather-fauzidea-flat-fauzidea.png`;
+        }
+        if(climate === `Sand`) {
+            climateImg.src = `https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/undefined/external-desert-landscape-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png`;
+        }
+        if(climate === `Ash`) {
+            climateImg.src = `https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/undefined/external-smoke-factory-flaticons-lineal-color-flat-icons-2.png`;
+        }
+        if(climate === `Squall`) {
+            climateImg.src = `https://img.icons8.com/fluency/96/undefined/waterspout.png`;
+        }
+        if(climate === `Tornado`) {
+            climateImg.src = `https://img.icons8.com/external-dreamcreateicons-outline-color-dreamcreateicons/64/undefined/external-tornado-weather-dreamcreateicons-outline-color-dreamcreateicons-2.png`;
+        }
     }
 }
 
